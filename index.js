85ee60e56f4440db06659cb4321a144787f7b6e3
栗城愛由 <kurikiaiyu@kurikiaiyunoMacBook-Air-2.local>
2022-03-12 22:45:22 +0900

    modified

--- index.js
+++ index.js
@@ -1,30 +1,68 @@
+
+
 $(function(){
-  let  firstPlace = $("#countA").text(); 
-  let countupA =() =>{
-    firstPlace++;
-  }
-
-  $("#start").onclick(function() {
-    setInterval(countupA,100);
-    $("#countA").text(firstPlace); 
-    
-    if(firstPlace >= 10){
-      let firstPlace = 0
+  
+  let milsec = 0
+  let sec = 0
+  let minute = 0
+  let hour = 0
+  
+  function timepassing(){
+    milsec++;
+    $("#countA").text(milsec);
+    if (milsec >= 10) {
+      sec++;
+      milsec = 0
+      $("#countA").text(milsec);
+      $("#countB").text(sec);
+       if ( sec >= 60) {
+         minute++;
+          sec = 0;
+        $("#countB").text(sec);
+        $("#countC").text(minute);
+        if ( minute >= 60) {
+          hour++;
+          minute = 0;
+          $("#countC").text(minute);
+          $("#countD").text(hour);
+        }
+      }
     }
+  }
+  function time(){
+    countTime=setInterval(function(){
+    timepassing()
+    } , 100);
+  }
+  function stopTimer(){
+    clearInterval(countTime);
+    }
+    
+
+  
+  $("#start").click(function() {
+    console.log("スタートボタン")
+    time();
   });
-});
+  $("#countA").text(milsec); 
+  $("#countB").text(sec); 
+  $("#countC").text(minute); 
+  $("#countD").text(hour); 
 
-$("#stop").onclick(function() {
-  clearInterval(firstPlace)
-  clearInterval(secondPlace)
-  clearInterval(thirdPlace)
-  clearInterval(fourthPlace)
-});
+    $("#stop").click(function() {
+      stopTimer();
+   });
 
-$("#reset").onclick(function() {
-  let firstPlace = 0
-  $("#countA").text(firstPlace); 
-  let secondPlace = 0
-  let thirdPlace = 0
-  let fourthPlace = 0
-});
+  $("#reset").click(function() {
+    stopTimer();
+    let milsec = 0
+    let sec = 0
+    let minute = 0
+    let hour = 0
+    $("#countA").text(milsec); 
+    $("#countB").text(sec); 
+    $("#countC").text(minute); 
+    $("#countD").text(hour);
+    });
+
+  });
